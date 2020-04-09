b01d8d4a6e8d6f0832b72e6c1275affe0b37b28d
Shen <shenbn@nci-02134517-ml.nih.gov>
2020-04-09 11:21:56 -0400

    implemented basic ls functionality

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-console.log('index.js loaded')
+//console.log('index.js loaded')
 const port = 8000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const open = require('open');
 const fetch = require("node-fetch");
+const prompt = require('prompt-sync')();
+
+const readline = require("readline");
 
 const client_id = '52zad6jrv5v52mn1hfy1vsjtr9jn5o1w'
 const client_secret = '2rHTqzJumz8s9bAjmKMV83WHX1ooN4kT'
@@ -32,7 +35,8 @@ async function getToken(code){
     .then(data => {
         tokenResponse = data
         token = tokenResponse.access_token
-        console.log(token)
+        //console.log(token)
+        cliLoop()
     })
 }
 let test = 'abc'
@@ -43,16 +47,17 @@ app.use(express.json({
 }))
 
 app.get('/',(req,res) => {
-    console.log(req.url)
+    //console.log(req.url)
     let url = req.url;
     let querystring = url.substring(url.indexOf('?') + 1)
     let params = readParms(querystring)
-    console.log(JSON.stringify(params))
+    //console.log(JSON.stringify(params))
     if(params.hasOwnProperty('code')){
-        console.log('CODE: '  + params.code)
+        //console.log('CODE: '  + params.code)
         getToken(params.code)
     }
     res.send('Thanks! You can close this browser now.')
+    server.close()
 })
 
 //app.get('/', (req, res) => res.send('Hello World! 1'))
@@ -66,9 +71,83 @@ expressApp.use(function(req, res, next){
     console.log(req);
     next();
 })*/
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+let server = app.listen(port, () => {})
 
 
 
 let url=`https://account.box.com/api/oauth2/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:8000/`
 open (url)
+
+async function cliLoop(){
+    let cmd = ""
+    let currFolder = '0'
+    while(cmd != "quit" && cmd != null){
+        cmd = prompt('>');
+        if(cmd != null){
+            let cmdParams = cmd.split(/\s+/)
+            //console.log(`Received:` + cmd);
+            if(cmdParams[0] == 'ls'){
+                if(cmdParams.length > 1){
+                    let r = await fetch('https://api.box.com/2.0/folders/' + cmdParams[1] + '/items',{
+                        method:"GET",
+                        headers: {
+                            "Authorization": "Bearer " + token
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        //console.log(JSON.stringify(data))
+                        //console.log(data)
+                        let folderNames = parseFolderItems(data)                
+                    })
+                }
+                else{
+                    let r = await fetch('https://api.box.com/2.0/folders/' + currFolder + '/items',{
+                        method:"GET",
+                        headers: {
+                            "Authorization": "Bearer " + token
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        //console.log(data)
+                        //console.log(JSON.stringify(data))
+                        let folderNames = parseFolderItems(data)                
+                    })
+                }
+            }
+        }
+        //cmd = "quit";
+        //console.log(cmd)
+    }
+}
+
+function parseFolderItems(response){
+    let items = response.entries;
+    let toReturn = [];
+    for(let i = 0; i < items.length; i++){
+        let item = items[i];
+        console.log(item.name)
+        toReturn.push(item.name)
+    }
+    return toReturn;
+}
+/*
+async function findFolder(folder){
+    let startFolder = currFolder;
+    if(folder.startsWith('/')){
+        startFolder = 
+    }
+}
+
+function nameToId(name, response ){
+    let items = response.entries;
+    let toReturn = [];
+    for(let i = 0; i < items.length; i++){
+        let item = items[i];
+        if(item.name == name){
+            return item.id;
+        }
+    }
+    return -1;
+}*/
